de523920251b50bf418abccb3671efc9435f2728
Pirogating <vitalilkvlasenko88@gmail.com>
2022-10-07 19:46:18 +0300

    done task0508

--- src/task0508/Solution.java
+++ src/task0508/Solution.java
@@ -12,14 +12,25 @@ public class Solution {
     public static void solution() {
         Scanner scanner = new Scanner(System.in);
 
-        strings = new String[6];
 
+        strings = new String[6];
         for (int i = 0; i < strings.length; i++) {
-            strings[i] = scanner.next();
+            strings[i] = scanner.nextLine();
         }
 
-        for (int i = 0; i < strings.length; i++) {
 
+        String currentString;
+        for (int i = 0; i < strings.length; i++) {
+            currentString = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (currentString == null) {
+                    break;
+                }
+                if (currentString.equals(strings[j])) {
+                    strings[j] = null;
+                    strings[i] = null;
+                }
+            }
         }
 
         for (String string : strings) {
