28e06aba0b052c2e9a13e30ac2a239910ab7bead
Pirogating <vitalilkvlasenko88@gmail.com>
2022-10-09 18:11:36 +0300

    done task0509

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Удаляем одинаковые строки
-        task0508.Solution.solution();
+//        // Удаляем одинаковые строки
+//        task0508.Solution.solution();
 
+
+        // Таблица умножения
+        task0509.Solution.solution();
     }
 }
--- src/task0509/Solution.java
+++ src/task0509/Solution.java
@@ -1,4 +1,39 @@
 package task0509;
 
+/*
+Таблица умножения
+https://javarush.ru/tasks/com.javarush.task.pro.task05.task0509
+*/
+
+
 public class Solution {
+
+    public static int[][] MULTIPLICATION_TABLE;
+
+    public static void solution() {
+        MULTIPLICATION_TABLE = new int[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            MULTIPLICATION_TABLE[0][i] = i + 1;
+        }
+
+        int tempNum;
+
+        for (int i = 1; i < MULTIPLICATION_TABLE.length; i++) {
+            tempNum = MULTIPLICATION_TABLE[i - 1][0] + 1;
+
+
+            for (int j = 1; j < MULTIPLICATION_TABLE.length + 1; j++) {
+                    MULTIPLICATION_TABLE[i][j - 1] = tempNum * j;
+            }
+        }
+
+
+        for (int[] ints : MULTIPLICATION_TABLE) {
+            for (int j = 0; j < MULTIPLICATION_TABLE.length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
