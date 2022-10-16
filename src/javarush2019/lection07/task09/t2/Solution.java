36db767d012d6aa1ac9014efdd6d5797621fc278
Pirogating <vitalikvlasenko88@gmail.com>
2022-10-16 17:22:22 +0300

    javarush2019.lection07.task09.t2

---
+++ src/javarush2019/lection07/task09/t2/Solution.java
@@ -0,0 +1,29 @@
+package javarush2019.lection07.task09.t2;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/*
+Слова в обратном порядке:
+
+Введи с клавиатуры 5 слов в список строк. Удали 3 - ий элемент списка, и выведи
+оставшиеся элементы в обратном порядке.
+ */
+
+public class Solution {
+    public static void solution() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        String temp;
+        for (int i = 0; i < 5; i++) {
+            temp = scanner.next();
+            list.add(temp);
+        }
+
+        list.remove(2);
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.println(list.get(i));
+        }
+    }
+}
