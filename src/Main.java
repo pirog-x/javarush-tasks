2de69017fdf7081eedfdfd486e851bd03144179c
Pirogating <vitalikvlasenko88@gmail.com>
2022-10-17 18:40:23 +0300

    done javarush2019.lection07.task12.normal.t7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        javarush2019.lection07.task12.normal.t6.Solution.solution();
+        javarush2019.lection07.task12.normal.t7.Solution.solution();
     }
 }
--- src/javarush2019/lection07/task12/normal/t7/Solution.java
+++ src/javarush2019/lection07/task12/normal/t7/Solution.java
@@ -1,4 +1,20 @@
 package javarush2019.lection07.task12.normal.t7;
 
+/*
+Переставь один модификатор static
+
+Переставь один модификатор static, чтобы пример скомпилировался.
+ */
+
 public class Solution {
+    public final static int A = 5;
+    public final static int B = 2;
+    public final static int C = A * B;
+
+    public static void solution() {
+    }
+
+    public int getValue() {
+        return C;
+    }
 }
