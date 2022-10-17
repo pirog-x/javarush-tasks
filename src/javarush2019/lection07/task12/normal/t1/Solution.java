a64dc62dfa64f0a5ce170790def6fbd94efb6f24
Pirogating <vitalikvlasenko88@gmail.com>
2022-10-17 16:46:42 +0300

    done javarush2019.lection07.task12.normal.t1

---
+++ src/javarush2019/lection07/task12/normal/t1/Solution.java
@@ -0,0 +1,25 @@
+package javarush2019.lection07.task12.normal.t1;
+import java.util.Scanner;
+import java.util.ArrayList;
+/*
+Вывести числа в обратном порядке
+
+Ввести с клавиатуры 10 чисел и заполнить ими список.
+Используя цикл for вывести их в обратном порядке.
+ */
+
+public class Solution {
+    public static void solution() {
+        ArrayList<Integer> list = new ArrayList<>(10);
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            int temp = scanner.nextInt();
+            list.add(temp);
+        }
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + ", ");
+        }
+    }
+}
