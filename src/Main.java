526443adaaa1aa309b73980b38e30aab045c1fbd
Pirogating <vitalikvlasenko88@gmail.com>
2022-10-17 18:34:53 +0300

    done javarush2019.lection07.task12.normal.t6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        javarush2019.lection07.task12.normal.t5.Solution.solution();
+        javarush2019.lection07.task12.normal.t6.Solution.solution();
     }
 }
