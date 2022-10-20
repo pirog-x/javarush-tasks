8061b498ba0e428c229f1d0ef3b3d97892f6750e
Pirogating <vitalikvlasenko88@gmail.com>
2022-10-20 12:49:32 +0300

    done javarush2019.level08.lesson08.t3 and modify *.t2

--- src/javarush2019/level08/lesson08/t3/Solution.java
+++ src/javarush2019/level08/lesson08/t3/Solution.java
@@ -1,36 +1,56 @@
 package javarush2019.level08.lesson08.t3;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Random;
+
+import java.util.Map;
+import java.util.Iterator;
+import java.util.HashMap;
 
 /*
-Удалить все числа больше 10
-
-Создать множество чисел(Set<Integer>), занести туда 20 различных чисел.
-Удалить из множества все числа больше 10.
+Одинаковые имя и фамилия
+Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия» - «Имя».
+Проверить сколько людей имеют совпадающие с заданным имя или фамилию.
  */
 
 public class Solution {
     public static void solution() {
-        Set<Integer> set = fillSetOfRandomNums(20, 30);
-        removeNumsInSetGreater(set, 10);
-
-        System.out.println(set);
+        Map<String, String> map = fillMap();
+        System.out.println("checkSameName(map) = " + checkSameName(map, "Виктор"));
+        System.out.println("checkSameName(map) = " + checkSameSurname(map, "Дуров"));
     }
 
-    private static Set<Integer> fillSetOfRandomNums(int size, int range) {
-        if (size <= 0)return null;
+    private static Map<String, String> fillMap() {
+        Map<String, String> map = new HashMap<>(10);
 
-        Random random = new Random();
-        Set<Integer> set = new HashSet<>(size);
+        map.put("Петров", "Петр");
+        map.put("Иванов", "Иван");
+        map.put("Василисов", "Виктор");
+        map.put("Жмуриков", "Антон");
+        map.put("Дуров", "Павел");
+        map.put("Костачкина", "Иван");
+        map.put("Александрович", "Виктор");
+        map.put("Пяточкин", "Илья");
+        map.put("Жильева", "Галина");
+        map.put("Трубочкин", "Иван");
 
-        for (int i = 0; i < size; i++) set.add(random.nextInt(range));
-        return set;
+        return map;
     }
 
-    private static void removeNumsInSetGreater(Set<Integer> set, int limit) {
-        if (set.isEmpty()) return;
-        Set<Integer> tempSet = Set.copyOf(set);
-        for (Integer i : tempSet) if (i > limit) set.remove(i);
+    private static int checkSameName(Map<String, String> map, String name) {
+        int countOfSameNames = 0;
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            if (iterator.next().getValue().equals(name)) countOfSameNames++;
+        }
+        return countOfSameNames;
+    }
+
+    private static int checkSameSurname(Map<String, String> map, String surname) {
+        int countSameSurnames = 0;
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            if (iterator.next().getKey().equals(surname)) countSameSurnames++;
+        }
+        return countSameSurnames;
     }
 }
